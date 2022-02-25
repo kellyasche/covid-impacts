b6e77421ea13f95d3f4d991fc7afa7de1b02b563
Kelly Asche <kellyasche@Kellys-Air.localdomain>
2022-02-25 09:42:20 -0600

    Started child care analysis

---
+++ Data/Child care/Master-child-care.R
@@ -0,0 +1,56 @@
+library(tidyverse)
+library(readxl)
+
+
+counties.regions <- read_csv("Data/Join docs/county_regions.csv") %>%
+  rename(mif = `MIF Region`) %>%
+  mutate(countyfp = formatC(countyfp, width = 3, flag = "0"),
+         Name = str_to_title(Name),
+         Name = str_replace(Name, "Q", "q"),
+         Name = str_replace(Name, "Of The", "of the"),
+         Name = str_replace(Name, "Mcleod", "McLeod"),
+         Dem_Desc = ifelse(Name == "Minnesota", "Minnesota", Dem_Desc) ,
+         edr = str_replace(edr, "  ", " "),
+         planning.region = str_replace(planning.region, " Minnesota", ""),
+         planning.region = fct_relevel(planning.region, "Northwest", "Northeast", "Central", "Seven County Mpls-St Paul", "Southwest", "Southeast"),
+         edr = fct_relevel(edr, "EDR 1 - Northwest", "EDR 2 - Headwaters", "EDR 3 - Arrowhead", "EDR 4 - West Central", "EDR 5 - North Central", "EDR 6E- Southwest Central", "EDR 6W- Upper Minnesota Valley", "EDR 7E- East Central", "EDR 7W- Central", "EDR 8 - Southwest", "EDR 9 - South Central", "EDR 10 - Southeast", "EDR 11 - 7 County Twin Cities", "Minnesota"),
+         mif = ifelse(is.na(mif), "TC", mif),
+         mif = as.factor(mif),
+         mif = fct_relevel(mif, "NW", "NE", "WC", "EC", "SW", "SE", "TC"))
+
+
+
+# Prep child care data ----------------------------------------------------
+
+est.need <- read_xlsx("Data/Child care/_Child Care Count and Capacity by County 2000-2020.xlsx", sheet = 1) %>%
+  rename(county = 1,
+         parents.lf.num = 2,
+         parents.lf.pct = 3) %>%
+  mutate(county = str_sub(county, 1, -18),
+         county = trimws(county, "both")) %>%
+  left_join(counties.regions, by = c("county" = "Name")) %>%
+  select(1,2,6,8:11)
+
+ccc.cap <- read_xlsx("Data/Child care/_Child Care Count and Capacity by County 2000-2020.xlsx", sheet = 4) %>%
+  rename(county = 1) %>%
+  select(1:22, 25:27) %>%
+  gather(key = "year", value = "ccc.capacity", 2:22) %>%
+  mutate(year = str_sub(year, - 2, -1),
+         year = paste("20", year, sep = ""),
+         year = as.numeric(year)) %>%
+  left_join(counties.regions[,c(1,2)], by = c("county" = "Name"))
+
+fcc.cap <- read_xlsx("Data/Child care/_Child Care Count and Capacity by County 2000-2020.xlsx", sheet = 6) %>%
+  rename(county = 1) %>%
+  select(1:22, 25:27) %>%
+  gather(key = "year", value = "fcc.capacity", 2:22) %>%
+  mutate(year = str_sub(year, -2, -1),
+         year = paste("20", year, sep = ""),
+         year = as.numeric(year))
+
+
+master <- est.need %>%
+  left_join(ccc.cap[,c()])
+
+names(ccc.cap)
+names(est.need)
